1c3a59062fdc6a90e4eb8555f2260bfe312d7208
Jaka Tertinek <tertinek.jaka@gmail.com>
2025-05-29 20:37:42 +0200

    Add loading animation

--- src/presentation/screens/SplashScreen.tsx
+++ src/presentation/screens/SplashScreen.tsx
@@ -1,22 +1,31 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { RouteName } from '../../navigation/RouteName';
 import { useStackNavigation } from '../../navigation/navigation';
+import LoadingAnimation from '../../assets/animations/loading.json';
+import LottieView from 'lottie-react-native';
+import { useDataContext } from '../../core/contexts/useDataContext';
+import { Colors } from '../../assets/styles/colors';
 
 const SplashScreen: React.FC = () => {
   const navigation = useStackNavigation();
+  const { isLoading } = useDataContext();
   useEffect(() => {
     const timer = setTimeout(() => {
-      // navigation.navigate(RouteName.HomeScreen);
+      navigation.navigate(RouteName.HomeScreen);
     }, 3000);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [isLoading]);
 
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="#007AFF" />
-      <Text style={styles.text}>Loading...</Text>
+      <LottieView
+        source={LoadingAnimation}
+        autoPlay
+        loop
+        style={styles.animation}
+      />
     </View>
   );
 };
@@ -26,12 +35,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: Colors.background,
   },
   text: {
     marginTop: 20,
     fontSize: 18,
     color: '#333',
+  },
+  animation: {
+    width: 200,
+    height: 200,
   },
 });
 
