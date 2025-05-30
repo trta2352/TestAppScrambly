cc61db7b83141875d895cd36dab35eeefe5361da
Jaka Tertinek <tertinek.jaka@gmail.com>
2025-05-30 18:59:09 +0200

    Rework App

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigator from './src/navigation/Navigator';
 import DataProvider from './src/core/contexts/useDataContext';
+import ErrorScreenOverlay from './src/presentation/components/error/ErrorScreenOverlay';
 
 const App = () => {
   return (
     <DataProvider>
+      <ErrorScreenOverlay />
       <Navigator />
     </DataProvider>
   );
